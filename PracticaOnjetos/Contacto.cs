374d50e7821d11651f81adca66b31a4845c6f52e
lizbethblu04 <lizbethblu04@hotmail.com>
2018-08-16 08:33:44 -0700

    Constructores

---
+++ PracticaOnjetos/Contacto.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticaObjetos
+{
+    class Contacto
+    {
+        //Autoproperties
+        public string Nombre { get; set; }
+        public string TelefonoCasa { get; set; }
+        public string TelefonoCelular { get; set; }
+        public string Direccion { get; set; }
+        public string CorreoElectronico { get; set; }
+
+        //Constructor
+        public Contacto()
+        {
+            Nombre = "Nuevo contacto";
+            TelefonoCasa = "(000) 000-000";
+        }
+
+        //Sobre carga para costructor
+        public Contacto( string nombre)
+        {
+            Nombre = nombre;
+        }
+        //se diferencia por el tipo de datos, no se puede repetir el mismo tipo de dato
+        public Contacto(string nombre, string telefonoCasa,string telefonoCelular, string direccion,string correoElectronico)
+        {
+            //los mayuscula es la propertie de la clase y los minuscula son los parametros del contructor
+            Nombre = nombre;
+            TelefonoCelular = telefonoCelular;
+            TelefonoCasa = telefonoCasa;
+            Direccion = direccion;
+            CorreoElectronico = correoElectronico;
+        }
+    }
+}
--- PracticaOnjetos/Program.cs
+++ PracticaOnjetos/Program.cs
@@ -9,19 +9,30 @@ namespace PracticaObjetos
     class Program
     {
         static void Main(string[] args)
-            {
-                Persona persona1 = new Persona();
-                persona1.nombre = "Juan";
-                persona1.apellidos = "Perez";
-                persona1.edad = 20;
-                Console.WriteLine(persona1.nombre + " " + persona1.apellidos + " tiene " + persona1.edad + " años.");
-                persona1.cumplirAños();
-                Console.WriteLine("Ahora " + persona1.nombre + " tiene " + persona1.edad + " años");
-                persona1.altura = 1.5f;
-                Console.WriteLine(persona1.nombre + " mide: " + persona1.altura);
-                persona1.crecer(0.12f);
-                Console.WriteLine(persona1.nombre + " ahora mide: " + persona1.altura);
-                Console.ReadLine();
-            }
+        {
+            Contacto contacto1 = new Contacto();
+
+            contacto1.Nombre = "Pedro Lopez";
+            contacto1.TelefonoCasa = "(644) 115 - 2514";
+            contacto1.Direccion = "Guerrero 430";
+
+            Console.WriteLine("Nombre: " + contacto1.Nombre);
+            Console.WriteLine("Telefono de casa: " + contacto1.TelefonoCasa);
+
+            Contacto contacto2 = new Contacto("Juan Perez");
+            Console.WriteLine("Contacto 2");
+            Console.WriteLine("Nombre: " + contacto2.Nombre);
+            Contacto contacto3 = new Contacto("Maria Navarro", "1234567890", "987654321", "5 de febrero 342", "mariaNava@correo.com");
+            Console.WriteLine("Contacto 3");
+            Console.WriteLine("Nombre: " + contacto3.Nombre);
+            Console.WriteLine("Celular: " + contacto3.TelefonoCelular);
+            Console.WriteLine("Casa: " + contacto3.TelefonoCasa);
+            Console.WriteLine("Direccion: " + contacto3.Direccion);
+            Console.WriteLine("Correo Electronico: " + contacto3.CorreoElectronico);
+
+            Console.ReadLine();
+
+            
+        }
     }
 }
